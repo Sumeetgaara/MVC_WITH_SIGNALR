e54ab0d7e259fa0ca2826fcb1d3dd06a7bb84e3f
sumbagaara <sumbagaara@gmail.com>
2018-07-01 18:26:15 +0530

    added

---
+++ MVCWITHSIGNALR/MyHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace MVCWITHSIGNALR
+{
+    public class MyHub : Hub
+    {
+        public void Send()
+        {
+            GlobalHost.ConnectionManager.GetHubContext<MyHub>().Clients.All.SendAsync("New Updates!Refresh to see!! ~Your Powerful Server.");
+        }
+
+    }
+}
